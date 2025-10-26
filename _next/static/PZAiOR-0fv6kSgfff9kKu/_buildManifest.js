bab788640f5199d4290411c60644c1c4fb82f0bc
Shobhit Bhatt <shobhit.bhatt.14@gmail.com>
2025-10-26 09:41:11 +0530

    Updates

--- _next/static/PZAiOR-0fv6kSgfff9kKu/_buildManifest.js
+++ _next/static/PZAiOR-0fv6kSgfff9kKu/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/shobhit-bhatt-14.github.io//_next/:path+",
-        "destination": "/shobhit-bhatt-14.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
